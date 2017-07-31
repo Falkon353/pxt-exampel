5cea6e496dae508013735fda1477fc2c854f53da
Martin Mostad <martin.mostad@ntebb.no>
2017-07-31 13:52:52 +0200

    Trying only c++

--- extension.cpp
+++ extension.cpp
@@ -5,6 +5,9 @@ using namespace pxt;
  */
 //% weight=1 color="#000000" icon="\uf2db"
 namespace exampel {
+    /**
+     * Description of the block
+     */
     //%blockId="stuff" block="haha %x| %y"
     int extfun(int x, int y) {
         return x + y;
--- shims.d.ts
+++ shims.d.ts
@@ -1,1 +1,17 @@
-// Will be auto-generated if needed.
+// Auto-generated. Do not edit.
+
+
+    /**
+     * Descritpion of a block
+     */
+    //% weight=1 color="#000000" icon="\uf2db"
+declare namespace exampel {
+
+    /**
+     * Description of the block
+     */
+    //%blockId="stuff" block="haha %x| %y" shim=exampel::extfun
+    function extfun(x: number, y: number): number;
+}
+
+// Auto-generated. Do not edit. Really.
