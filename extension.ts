fe6e473e41e7c7c23f7fc3dc3548878d6a15b79f
Martin Mostad <martin.mostad@ntebb.no>
2017-07-31 13:23:01 +0200

    Testing example

---
+++ extension.ts
@@ -0,0 +1,10 @@
+namespace exampel {
+    /**
+     * Help goes here.
+     */
+    //% shim=exampel::extfun
+    export function extfun(x: number, y: number) {
+        // Dummy implementation for the simulator.
+        return x - y
+    }
+}
