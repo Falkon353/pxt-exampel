ffb16695e8bb48e2572020853697f6a24812ab67
Martin Mostad <martin.mostad@ntebb.no>
2017-07-31 14:21:40 +0200

    Trying void function

--- extension.cpp
+++ extension.cpp
@@ -3,7 +3,7 @@ using namespace pxt;
 /**
  * Descritpion of a block
  */
-//% weight=1 color="#000000" icon="\uf2db"
+//% weight=1 color="#550000" icon="\uf2db"
 namespace exampel {
     /**
      * Description of the block
@@ -12,4 +12,11 @@ namespace exampel {
     int extfun(int x, int y) {
         return x + y;
     }
+    /**
+     * Shows a simpel number
+     */
+     //%blockId="showStuff" block="Number %num"
+     void showStuff(int num){
+         uBit.display.image.setPixelValue(0, 0, 1);
+     }
 }
--- shims.d.ts
+++ shims.d.ts
@@ -4,7 +4,7 @@
     /**
      * Descritpion of a block
      */
-    //% weight=1 color="#000000" icon="\uf2db"
+    //% weight=1 color="#550000" icon="\uf2db"
 declare namespace exampel {
 
     /**
@@ -12,6 +12,12 @@ declare namespace exampel {
      */
     //%blockId="stuff" block="haha %x| %y" shim=exampel::extfun
     function extfun(x: number, y: number): number;
+
+    /**
+     * Shows a simpel number
+     */
+    //%blockId="showStuff" block="Number %num" shim=exampel::showStuff
+    function showStuff(num: number): void;
 }
 
 // Auto-generated. Do not edit. Really.
